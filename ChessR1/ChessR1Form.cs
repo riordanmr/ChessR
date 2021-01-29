97449a149a1e1119bbf0ddeee60a028f94c1cd8a
Mark Riordan <riordan@progress.com>
2021-01-29 14:49:02 -0700

    Pawn Moves & code autoformat

--- ChessR1/ChessR1Form.cs
+++ ChessR1/ChessR1Form.cs
@@ -8,7 +8,6 @@
 
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Diagnostics;
 using System.Drawing;
 using System.Text;
@@ -29,6 +28,11 @@ namespace ChessR1
         System.Drawing.Pen penBlack;
         Pen penSelectedStart = new Pen(Color.MediumSpringGreen, 2.0F);
         Pen penLegalMoves = new Pen(Color.Plum, 2.0F);
+        // I'm using Unicode characters to render the pieces.  
+        // I don't think that all the different fonts each has a unique rendering of 
+        // the pieces, but I can see that at least Arial and Segoe UI Symbol do.
+        // Unfortunately, the center points of the characters differ between those
+        // fonts, so you can't just swap in one font for another and have it look good. 
         Font fontPieces = new Font("Arial", 60);
         Font fontCoords = new Font("Lucida Console", 24);
         Board m_board = new Board();
@@ -38,6 +42,7 @@ namespace ChessR1
         int selectedRowStart = NOT_SELECTED, selectedColStart = NOT_SELECTED;
         byte[] m_ValidMoves = new byte[64];
         int m_nValidMoves;
+        bool bWhiteOnBottom = true;
 
 
         public ChessR1Form() {
@@ -64,18 +69,22 @@ namespace ChessR1
             icol = rowcol / NUMCOLS;
         }
 
+        int ColorOfCell(byte cell) {
+            return (cell & (int)PieceColor.Mask);
+        }
+
         // Drawing a pawn by computing all the geometric shapes, and trying to 
         // have a border (by following Draw with Fill) is really hard.
         // And the results look surprisingly grainy.  
         // I have abandoned this approach for now.
         private void DrawPawnBad(Graphics g, int irow, int icol) {
-            float x = (float) (offsetLeft + icol * squareSize + 0.42 * squareSize);
-            float y = (float) (offsetTop + irow * squareSize + 0.25 * squareSize);
+            float x = (float)(offsetLeft + icol * squareSize + 0.42 * squareSize);
+            float y = (float)(offsetTop + irow * squareSize + 0.25 * squareSize);
             float diameter = (float)(squareSize * 0.2);
-            
+
             g.DrawEllipse(penBlack, x, y, diameter, diameter);
-            g.FillEllipse(brushWhite, (float)(x + 0.5*thickness), (float)(y + 0.5*thickness),
-                (float)(diameter - 1*thickness), (float)(diameter - 1*thickness));
+            g.FillEllipse(brushWhite, (float)(x + 0.5 * thickness), (float)(y + 0.5 * thickness),
+                (float)(diameter - 1 * thickness), (float)(diameter - 1 * thickness));
             PointF[] pointsTriangle = new PointF[3];
 
             x = (float)(offsetLeft + icol * squareSize + 0.5 * squareSize);
@@ -103,8 +112,8 @@ namespace ChessR1
             string strPiece = strPieces.Substring(pieceWithColor, 1);
             SizeF textSize = g.MeasureString(strPiece, fontPieces);
             PointF textSizeF = textSize.ToPointF();
-            float x = (float)(offsetLeft + squareSize * icol + 0.0*textSizeF.X);
-            float y = (float)(offsetTop + squareSize * irow + 0.2*textSizeF.Y);
+            float x = (float)(offsetLeft + squareSize * icol + 0.0 * textSizeF.X);
+            float y = (float)(offsetTop + squareSize * irow + 0.2 * textSizeF.Y);
             g.DrawString(strPiece, fontPieces, brushBlack, x, y);
         }
 
@@ -133,22 +142,24 @@ namespace ChessR1
             string strNumbers = "12345678";
             for (int irow = 0; irow < 8; irow++) {
                 float x = (float)(offsetLeft - squareSize * 0.4);
-                float y = (float)(offsetTop + squareSize * (irow + 0.5) - textSize.Height*0.5);
-                g.DrawString(strNumbers.Substring(7-irow, 1), fontCoords, brushWhite, x, y);
+                float y = (float)(offsetTop + squareSize * (irow + 0.5) - textSize.Height * 0.5);
+                g.DrawString(strNumbers.Substring(7 - irow, 1), fontCoords, brushWhite, x, y);
             }
         }
 
         // Draw colored rectangles in the squares we want to highlight.
         // The square currently selected by the user (if any) gets a green rectangle.
         void DrawHighlights(Graphics g) {
+            // Highlight the currently selected square, if any.
             if (selectedRowStart >= 0 && selectedColStart >= 0) {
                 float x = (float)(offsetLeft + (selectedColStart + 0.05) * squareSize);
                 float y = (float)(offsetTop + (selectedRowStart + 0.05) * squareSize);
-                float width = (float)(squareSize*0.9);
+                float width = (float)(squareSize * 0.9);
                 float height = width;
                 g.DrawRectangle(penSelectedStart, x, y, width, height);
             }
 
+            // Highlight the squares to which the currently selected piece can move.
             int irow, icol;
             for (int idx = 0; idx < m_nValidMoves; idx++) {
                 CalcRowAndColFromByte(m_ValidMoves[idx], out irow, out icol);
@@ -160,11 +171,73 @@ namespace ChessR1
             }
         }
 
-        int ComputeLegalMovesForRook(int irow, int icol, ref byte [] aryValidMoves) {
+        int ComputeLegalMovesForPawn(int irow, int icol, ref byte[] aryValidMoves) {
+            int nMoves = 0;
+
+            int piece = m_board.cells[irow, icol];
+            int myColor = piece & PieceColor.Mask;
+            if (PieceColor.White == myColor && bWhiteOnBottom || PieceColor.Black == myColor && !bWhiteOnBottom) {
+                // Moves are from bottom to top.
+                // This check shouldn't really be necessary, since a pawn can't be on the last rank.
+                if (irow > 0) {
+                    if (0 == m_board.cells[irow - 1, icol]) {
+                        // Square is empty.
+                        aryValidMoves[nMoves++] = CalcByteFromRowAndCol(irow - 1, icol);
+                    }
+                    if (irow == NUMROWS - 2) {
+                        // Pawn is in initial position, so move of 2 squares may be available.
+                        if (0 == m_board.cells[irow - 1, icol] && 0 == m_board.cells[irow - 2, icol]) {
+                            aryValidMoves[nMoves++] = CalcByteFromRowAndCol(irow - 2, icol);
+                        }
+                    }
+                    // Now for captures. 
+                    if (icol > 0) {
+                        if (0 != m_board.cells[irow - 1, icol - 1] && ColorOfCell(m_board.cells[irow - 1, icol - 1]) != myColor) {
+                            aryValidMoves[nMoves++] = CalcByteFromRowAndCol(irow - 1, icol - 1);
+                        }
+                    }
+                    if (icol < NUMCOLS - 1) {
+                        if (0 != m_board.cells[irow - 1, icol + 1] && ColorOfCell(m_board.cells[irow - 1, icol + 1]) != myColor) {
+                            aryValidMoves[nMoves++] = CalcByteFromRowAndCol(irow - 1, icol + 1);
+                        }
+                    }
+                }
+            } else {
+                // Moves are from top to bottom.
+                // Move one ahead?
+                if (irow < NUMROWS-1) {
+                    if (0 == m_board.cells[irow + 1, icol]) {
+                        // Square is empty.
+                        aryValidMoves[nMoves++] = CalcByteFromRowAndCol(irow + 1, icol);
+                    }
+                    if (irow == 1) {
+                        // Pawn is in initial position, so move of 2 squares may be available.
+                        if (0 == m_board.cells[irow + 1, icol] && 0 == m_board.cells[irow + 2, icol]) {
+                            aryValidMoves[nMoves++] = CalcByteFromRowAndCol(irow + 2, icol);
+                        }
+                    }
+                    // Now for captures. 
+                    if (icol > 0) {
+                        if (0 != m_board.cells[irow + 1, icol - 1] && ColorOfCell(m_board.cells[irow + 1, icol - 1]) != myColor) {
+                            aryValidMoves[nMoves++] = CalcByteFromRowAndCol(irow + 1, icol - 1);
+                        }
+                    }
+                    if (icol < NUMCOLS - 1) {
+                        if (0 != m_board.cells[irow + 1, icol + 1] && ColorOfCell(m_board.cells[irow + 1, icol + 1]) != myColor) {
+                            aryValidMoves[nMoves++] = CalcByteFromRowAndCol(irow + 1, icol + 1);
+                        }
+                    }
+                }
+            }
+
+            return nMoves;
+        }
+
+        int ComputeLegalMovesForRook(int irow, int icol, ref byte[] aryValidMoves) {
             int nMoves = 0;
             int ir, ic;
-            int pieceType = m_board.cells[irow, icol];
-            int myColor = pieceType & PieceColor.Mask;
+            int piece = m_board.cells[irow, icol];
+            int myColor = piece & PieceColor.Mask;
             int otherColor, otherPiece;
             // Look in same row.
             // First, look to the left.
@@ -247,10 +320,13 @@ namespace ChessR1
         int ComputeLegalMovesForPiece(int irow, int icol) {
             int pieceType = m_board.cells[irow, icol];
             pieceType &= PieceType.Mask;
-            int nMoves=0;
+            int nMoves = 0;
             switch (pieceType) {
                 case PieceType.Rook:
                     nMoves = ComputeLegalMovesForRook(irow, icol, ref m_ValidMoves);
+                    break;
+                case PieceType.Pawn:
+                    nMoves = ComputeLegalMovesForPawn(irow, icol, ref m_ValidMoves);
                     break;
             }
             return nMoves;
@@ -334,14 +410,17 @@ namespace ChessR1
             //    (float)(offsetLeft + 0.15 * squareSize), (float)(offsetTop + 6.15 * squareSize));
 
             CreateInitialBoard(ref m_board);
-            // Sample piece on board - temporary.
-            m_board.cells[3, 3] = PieceType.Rook | PieceColor.White;
+            // Sample pieces on board - temporary.
+            m_board.cells[4, 3] = PieceType.Rook | PieceColor.White;
+            m_board.cells[2, 3] = PieceType.Bishop | PieceColor.White;
+            m_board.cells[5, 3] = PieceType.Knight | PieceColor.White;
+            m_board.cells[5, 5] = PieceType.Knight | PieceColor.Black;
             DrawBoard(e.Graphics, ref m_board);
         }
 
         private void ChessR1Form_MouseDown(object sender, MouseEventArgs e) {
-            // Implicit conversion from Point to PointF.
             selectedColStart = NOT_SELECTED;
+            // Implicit conversion from Point to PointF.
             PointF pointMouse = e.Location;
             if (pointMouse.X >= offsetLeft && pointMouse.X < offsetLeft + NUMCOLS * squareSize) {
                 if (pointMouse.Y > offsetTop && pointMouse.Y < offsetTop + NUMROWS * squareSize) {
